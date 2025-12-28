723893d9f8353ce0fbde4e8cd7f6e3f7905d8519
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2025-12-28 11:45:43 +0530

    feat: implement CreatePost command and PostController for post creation functionality

---
+++ uni-connect-api/API/Controllers/PostController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Post.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/posts")]
+//[Authorize]
+public class PostController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    
+    public PostController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreatePost([FromForm] CreatePost command)
+    {
+        
+        var res = await _mediator.Send(command);
+        return Ok(res);
+    }
+}
